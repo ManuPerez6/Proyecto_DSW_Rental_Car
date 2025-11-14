28b4b7d72e9ef978e0831217b237867a51f0bfd0
ManuPerez6 <mperezjijon@frvt.utn.edu.ar>
2025-11-14 22:47:37 +0000

    Agregado form para añadir vehiculos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,43 +1,27 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 import { CarListComponent } from './components/car-list/car-list.component';
 import { CarDetailComponent } from './components/car-detail/car-detail.component';
 import { CarFormComponent } from './components/car-form/car-form.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
+
 import { authGuard } from './auth/auth.guard';
 import { adminGuard } from './auth/admin.guard';
 
 export const routes: Routes = [
-  // 1. RUTAS PÚBLICAS
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   
-  // 2. RUTAS PARA USUARIOS LOGUEADOS (user y admin)
-  {
-    path: 'car',
-    component: CarListComponent,
-    canActivate: [authGuard]
-  },
-  {
-    path: 'car/:id',
-    component: CarDetailComponent,
-    canActivate: [authGuard]
-  },
+  { path: 'car', component: CarListComponent },
+  { path: 'car/detail/:id', component: CarDetailComponent, canActivate: [authGuard] },
 
-  // 3. RUTAS SOLO PARA ADMIN
-  {
-    path: 'car/new',
+  { 
+    path: 'car/new', 
     component: CarFormComponent,
-    canActivate: [adminGuard] 
+    canActivate: [adminGuard]
   },
-  {
-    path: 'car/:id/edit',
-    component: CarFormComponent,
-    canActivate: [adminGuard] 
-  },
-
-  // 4. RUTA WILDCARD
+  
   { path: '**', redirectTo: '' }
 ];
